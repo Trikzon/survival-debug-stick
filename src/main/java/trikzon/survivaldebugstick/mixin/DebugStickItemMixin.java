8f96e5c01c6959653f0294145a1f729d9bc66036
Trikzon <trikzongaming@gmail.com>
2020-01-25 23:21:45 -0500

    Change license to LGPL 3 or later

--- src/main/java/trikzon/survivaldebugstick/mixin/DebugStickItemMixin.java
+++ src/main/java/trikzon/survivaldebugstick/mixin/DebugStickItemMixin.java
@@ -1,3 +1,24 @@
+/* ============================================================================
+ * Copyright 2020 Trikzon
+ *
+ * Survival-Debug-Stick is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * File: DebugStickItemMixin.java
+ * Date: 2020-01-25
+ * Revision:
+ * Author: Trikzon
+ * ============================================================================ */
 package trikzon.survivaldebugstick.mixin;
 
 import net.minecraft.entity.player.PlayerEntity;
@@ -8,13 +29,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(DebugStickItem.class)
-public abstract class DebugStickItemMixin extends Item {
-    public DebugStickItemMixin(Settings item$Settings_1) {
+public abstract class DebugStickItemMixin extends Item
+{
+    public DebugStickItemMixin(Settings item$Settings_1)
+    {
         super(item$Settings_1);
     }
 
     @Redirect(method = "use", at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/player/PlayerEntity;isCreativeLevelTwoOp()Z"))
-    public boolean isCreativeLevelTwoOpProxy(PlayerEntity playerEntity_1) {
+    public boolean isCreativeLevelTwoOpProxy(PlayerEntity playerEntity_1)
+    {
         return true;
     }
 }
--- src/main/java/trikzon/survivaldebugstick/mixin/ServerPlayerInteractionManagerMixin.java
+++ src/main/java/trikzon/survivaldebugstick/mixin/ServerPlayerInteractionManagerMixin.java
@@ -1,3 +1,24 @@
+/* ============================================================================
+ * Copyright 2020 Trikzon
+ *
+ * Survival-Debug-Stick is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * File: ServerPlayerInteractionManagerMixin.java
+ * Date: 2020-01-25
+ * Revision:
+ * Author: Trikzon
+ * ============================================================================ */
 package trikzon.survivaldebugstick.mixin;
 
 import net.minecraft.block.BlockState;
@@ -11,13 +32,17 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(ServerPlayerInteractionManager.class)
-public abstract class ServerPlayerInteractionManagerMixin {
+public abstract class ServerPlayerInteractionManagerMixin
+{
 
     @Redirect(method = "method_14263", at = @At(value = "INVOKE", target = "Lnet/minecraft/block/BlockState;calcBlockBreakingDelta(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/world/BlockView;Lnet/minecraft/util/math/BlockPos;)F"))
-    public float calcBlockBreakingDeltaProxy(BlockState blockState_1, PlayerEntity playerEntity_1, BlockView blockView_1, BlockPos blockPos_1) {
-        if (playerEntity_1.getMainHandStack().getItem().equals(Items.DEBUG_STICK)) {
+    public float calcBlockBreakingDeltaProxy(BlockState blockState_1, PlayerEntity playerEntity_1, BlockView blockView_1, BlockPos blockPos_1)
+    {
+        if (playerEntity_1.getMainHandStack().getItem().equals(Items.DEBUG_STICK))
+        {
             return 1.0F;
-        } else {
+        } else
+        {
             return blockState_1.calcBlockBreakingDelta(playerEntity_1, blockView_1, blockPos_1);
         }
     }
